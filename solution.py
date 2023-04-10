5c8e4946d0fb088b4cac17421e7c43578ae8ff81
fruittic <129435665+fruittic@users.noreply.github.com>
2023-04-10 22:16:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 224851402 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.06
+    return stats.ks_2samp(x, y).pvalue < alpha
