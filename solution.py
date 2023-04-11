f7800b77a193e9f21163abfae55f2bb06e6d3302
fruittic <129435665+fruittic@users.noreply.github.com>
2023-04-11 12:17:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
+from hyppo.ksample import MMD
 
 chat_id = 224851402 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.06
-    return stats.ks_2samp(x, y).pvalue < alpha
+    return MMD(compute_kernel="rbf", gamma=1).test(x, y)[1] < alpha
